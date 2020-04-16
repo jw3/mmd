60a842530c3d7d01eeb2ee495dc795cecd82f172
John Wass <wassj@ctc.com>
2020-04-16 17:09:48 -0400

    clean up the vr mem

--- src/main/scala/polyform/modbus/MemoryVR.scala
+++ src/main/scala/polyform/modbus/MemoryVR.scala
@@ -1,45 +1,40 @@
 package polyform.modbus
 
 import akka.actor.{Actor, ActorLogging, Props}
+import com.ctc.polyform.Protocol.CellZ
 import polyform.Controller.AsIsMoveEvent
-import polyform.modbus.MemoryVR.{MemMap, ModifyVR, RequestVR, VR}
+import polyform.modbus.MemoryVR._
 
 object MemoryVR {
   private type XY = (Int, Int)
+  private type MemMap = Map[XY, Int]
+
   def props(): Props = Props(new MemoryVR)
 
   def xy(x: Int, y: Int): Int = y << 6 | x
   def x(vr: Int): Int = vr & 0x003F
   def y(vr: Int): Int = vr >> 6 & 0x003F
-  def x_y(vr: Int): String = s"${x(vr)}_${y(vr)}"
+  def x_y(vr: Int): XY = x(vr) -> y(vr)
+  def x_y(cz: CellZ): XY = cz.x -> cz.y
 
   case class ModifyVR(address: Int, value: Int)
   case class RequestVR(address: Int)
   case class VR(value: Int)
-
-  type MemMap = Map[XY, Int]
-  def table(): MemMap = {
-    val vrs = for {
-      xx <- 0 until 63
-      yy <- 0 until 63
-    } yield (xx, yy) -> 0
-    vrs.toMap
-  }
 }
 
 class MemoryVR extends Actor with ActorLogging {
-  var table: MemMap = MemoryVR.table()
+  var table: MemMap = Map.empty
 
   def receive: Receive = {
     case ModifyVR(address, value) =>
-      table += (MemoryVR.x(address) -> MemoryVR.y(address)) -> value
+      table += x_y(address) -> value
       sender ! VR(value)
 
     case RequestVR(address) ⇒
-      table.get(MemoryVR.x(address) -> MemoryVR.y(address)).foreach { value =>
-        sender ! VR(value)
-      }
+      val value = table.getOrElse(x_y(address), 0)
+      sender ! VR(value)
+
     case AsIsMoveEvent(_, cz) =>
-      table += (cz.x, cz.y) -> cz.z
+      table += x_y(cz) -> cz.z
   }
 }
